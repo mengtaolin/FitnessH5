8c73d07a6327cc76a0d4af7b38c48929373e401f
mengtaolin <1187352588@qq.com>
2018-04-08 01:55:05 +0800

    添加播放按钮，用以控制音乐内容
    v0.03添加播放按钮，用以控制音乐内容

--- js/android.js
+++ js/android.js
@@ -1,9 +1,15 @@
-(function(app, document) {
+(function(app, document, window) {
+	var itemList = [];
 	app.playMusic = function(event){
+		var ele = event.currentTarget;
+		var fType = ele.getAttribute("ftype");
+		
 		event.stopImmediatePropagation();
 		event.stopPropagation();
 		event.cancelBubble = true;
-		app.test(null);
+		event.bubbles = false;
+		event.preventDefault();
+		alert("playMusic");
 	}
 	
 	app.test = function(str){
@@ -11,18 +17,48 @@
 		alert(str);
 	}
 	
+	app.playSong = function(event){
+		window.android.playSong();
+	}
+	
+	app.preSong = function(event){
+		window.android.preSong();
+	}
+	
+	app.nextSong = function(event){
+		window.android.nextSong();
+	}
+	
 	app.addItem = function(e){
-		var li = app.createElement("li", {"class":"mui-table-view", "onclick": "app.onItemClick(event)"}, "");
+		var ul = app.createElement("ul",{"class":"mui-table-view", "id":itemList.length},"")
+		var li = app.createElement("li", {"class":"mui-table-view-cell mui-media", "onclick": "app.onItemClick(event)"}, "");
+		var a = app.createElement("a", {}, "");
 		var img = app.createElement("img", {"class":"mui-media-object mui-pull-left", "src":"http://placehold.it/40x30"}, "");
-		li.appendChild(img);
+		a.appendChild(img);
 		var div = app.createElement("div", {"class":"mui-media-body"}, "幸福");
 		var p = app.createElement("p", {"class":"mui-ellipsis"}, "能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？");
 		div.appendChild(p);
-		var span = app.createElement("span", {"class":"mui-icon iconfont icon-swticonyinle3", "onclick":"app.test(null)"}, "");
-		div.appendChild(span);
-		li.appendChild(div);
-		var ul = document.getElementById("musicList");
+		var btnsDiv = app.createElement("div", {"class":"mui-button-row mui-pull-left"}, "")
+//		var musicSpan = app.createElement("div", {"class":"mui-icon iconfont icon-14", "onclick":"app.test(null)", "ftype":"1"}, "");
+		var preSongDiv = app.createElement("div", {"class":"mui-icon iconfont icon-shangyishou", "onclick":"app.preSong(event)", "ftype":"1"}, "");
+		var playSongDiv = app.createElement("div", {"class":"mui-icon iconfont icon-bofanganniu", "onclick":"app.playSong(event)", "ftype":"1", "isPlay":false}, "");
+		var nextSongDic = app.createElement("div", {"class":"mui-icon iconfont icon-xiayishou", "onclick":"app.nextSong(event)", "ftype":"1"}, "");
+		btnsDiv.appendChild(preSongDiv);
+		btnsDiv.appendChild(playSongDiv);
+		btnsDiv.appendChild(nextSongDic);
+		div.appendChild(btnsDiv);
+		a.appendChild(div);
+		li.appendChild(a);
+		var mainUl = document.getElementById("musicList");
 		ul.appendChild(li);
+		mainUl.appendChild(ul);
+		
+		app.pushItem(li, a, img, preSongDiv, playSongDiv, nextSongDic);
+	}
+	
+	app.pushItem = function(li, a, img, preSongDiv, playSongDiv, nextSongDiv){
+		var obj = {"li":li, "a":a, "img":img, "pre":preSongDiv, "play":playSongDiv, "next":nextSongDiv};
+		itemList.push(obj);
 	}
 	
 	app.createElement = function(type, classValue, innerHtml){
@@ -34,10 +70,17 @@
 		return ele;
 	}
 	
-	app.onItemClick = function(e){
-		alert("item click")
-		e.stopImmediatePropagation();
-		e.stopPropagation();
+	app.onItemClick = function(event){
+		var ele = event.target;
+		var ftype = ele.hasAttribute("ftype");
+		if(ftype)return;
+		alert("item click");
+		event.bubbles = false;
+		event.stopImmediatePropagation();
+		event.stopPropagation();
 		event.cancelBubble = true;
+		event.preventDefault();
 	}
-})(window.app = {}, document)
+	
+	
+})(window.app = {}, document, window)
